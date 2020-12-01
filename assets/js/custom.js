aa50eb9fde53d3c238f26537e31a79d8891c5a61
sadeiasturias <73172809+sadeiasturias@users.noreply.github.com>
2020-12-01 13:09:39 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,9 +3,14 @@ opensdg.dataRounding = function(value) {
     return value
   }
   else {
-    return value.toFixed(1).replace('.', ',') // returns a character
-    // Sentences below return a number:
-    // return Number(value.toFixed(1))
-    // return Number(value.toPrecision(3))
+    return value.toFixed(1) // returns a character (e.g. "13.0")
   }
 };
+
+// Charting library jqPlot seems not to accept comma as a decimal separator,
+// we therefore cannot use the following:
+	// value.toFixed(1).replace('.', ',')
+
+// Sentences below return a number (in which decimal zeroes disappear):
+    // Number(value.toFixed(1))
+    // Number(value.toPrecision(3))
