e8363b8f715590b1e809f57d4436d00044a054ed
sadeiasturias <73172809+sadeiasturias@users.noreply.github.com>
2020-12-09 11:59:56 +0000

    Deploying to gh-pages from  @ b82982ce44fa58bc215ee41c6e6438d8d38ebe07 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,15 +3,16 @@ opensdg.dataRounding = function(value) {
     return value
   }
   else {
-    return value.toFixed(1)
-    // returns a character (e.g. "13.0")
-    // For mose reason, "0.0" is still shown as "0" in the platform tables
+    return value.toLocaleString('es-ES', {maximumFractionDigits: 1, minimumFractionDigits: 1})
+    // Returns a character (e.g. "13.0").
+    // For mose reason, "0.0" is still shown as "0" in tables on the web.
   }
 };
 
 // Charting library jqPlot seems not to accept comma as a decimal separator,
 // we therefore cannot use the following:
 	// value.toFixed(1).replace('.', ',')
+	// Number(value.toFixed(1)).toLocaleString('es-ES')
 
 // Sentences below return a number (in which decimal zeroes disappear):
     // Number(value.toFixed(1))
