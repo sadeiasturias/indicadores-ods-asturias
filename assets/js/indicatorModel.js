41ff50a18e484a0058374b5160ccde46e2614da1
Sadei <web@sadei.es>
2021-11-26 13:22:16 +0100

    Added code from indicator Model.js

--- assets/js/indicatorModel.js
+++ assets/js/indicatorModel.js
@@ -1,26 +1,3 @@
-opensdg.dataRounding = function(value) {
-  if (value == null) {
-    return value
-  }
-  else {
-    return value.toFixed(1)
-    // Returns a character (e.g. "13.0").
-    // For mose reason, "0.0" is still shown as "0" in tables on the web.
-  }
-};
-
-// Charting library jqPlot seems not to accept comma as a decimal separator,
-// we therefore cannot use the following:
-	// value.toFixed(1).replace('.', ',')
-	// Number(value.toFixed(1)).toLocaleString('es-ES')
-	// value.toLocaleString('es-ES', {maximumFractionDigits: 1, minimumFractionDigits: 1})
-
-// Sentences below return a number (in which decimal zeroes disappear):
-    // Number(value.toFixed(1))
-    // Number(value.toPrecision(3))
-	
-//{%- include assets/js/indicatorModel.js -%}
-
 var indicatorModel = function (options) {
 
   var helpers = {% include assets/js/model/helpers.js %}
